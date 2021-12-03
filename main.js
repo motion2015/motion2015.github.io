1e2001557688b940a0d47490e853a5d097792ef9
motion2015 <motion74@naver.com>
2021-12-03 13:22:47 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+window.onload = () =>{
+    let scrollpos = window.scrollY; 
+    console.log(scrollpos);
+    const about = document.querySelector("#about > div")
+    const tech = document.querySelector("#Tech > div")
+    const awards = document.querySelector("#awards > div") 
+    const add_class_on_scroll = (dom) => dom.classList.add("come-in")  
+    window.addEventListener('scroll', function() { 
+      scrollpos = window.scrollY; 
+      if (scrollpos >= about.offsetHeight - 200) add_class_on_scroll(about)
+      if (scrollpos >= tech.offsetHeight + 300) add_class_on_scroll(tech)
+      if (scrollpos >= awards.offsetHeight + 700) add_class_on_scroll(awards) 
+    }) 
+} 
